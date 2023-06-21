9c03281e8265e1ba5902d2d36bd002489745f5e2
giovannapfs <giovanna.figueirasilva@gmail.com>
2023-06-21 17:08:21 -0300

    arrumando script.js e mudando a cor de fundo no style.css

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,15 +2,15 @@ const horas = document.getElementById('horas');
 const minutos = document.getElementById('minutos');
 const segundos = document.getElementById('segundos');
 
-const relogio =setInterval(function time() {
+const relogio = setInterval(function time() {
     let dateToday = new Date();
-    let hr =dateToday.getHours();
-    let min =dateToday.getMinutes();
-    let s =dateToday.getSeconds();
+    let hr = dateToday.getHours();
+    let min = dateToday.getMinutes();
+    let s = dateToday.getSeconds();
 
 if (hr < 10) hr = '0' + hr;
-if (min < 10) hr = '0' + min;
-if (s < 10) hr = '0' + s;
+if (min < 10) min = '0' + min;
+if (s < 10) s = '0' + s;
 
 
     horas.textContent = hr;
